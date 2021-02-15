72264263991c6fcaaa5da1690cec7a08f4759528
wwkarev <wwkarev@gmail.com>
2021-02-15 17:44:04 +0700

    added prefix url

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,8 +1,11 @@
 import express from 'express'
 import userActivity from './userActivity'
+import dotenv from 'dotenv'
+
+dotenv.config()
 
 const configureRoutes = (app: express.Application) => {
-    app.use('/user_activity', userActivity)
+    app.use(`${process.env.PREFIX_URL}/user_activity`, userActivity)
 }
 
 export default configureRoutes
